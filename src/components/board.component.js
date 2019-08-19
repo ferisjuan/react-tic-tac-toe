806c0fbdf264b45c4073ea5bcec3334819822d39
ferisjuan <feris.juan@gmail.com>
2019-08-19 11:37:48 -0500

    Noew the board is written by two cicles

--- src/components/board.component.js
+++ src/components/board.component.js
@@ -4,27 +4,30 @@ import Square from './square.component';
 
 export default class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square value={this.props.squares[i]} key={i} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
+    const rows = Array(3).fill(null);
+    const cols = Array(3).fill(null);
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows.map((row, j) => {
+          return (
+            <div className="board-row" key={j}>
+              {cols.map((col, i) => {
+                if (j === 0) {
+                  return this.renderSquare(i);
+                } else if (j === 1) {
+                  return this.renderSquare(i + 3);
+                } else {
+                  return this.renderSquare(i + 6);
+                }
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
