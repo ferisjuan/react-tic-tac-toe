a6c943e1e1541d2065e012791fce1f777c322a27
ferisjuan <feris.juan@gmail.com>
2019-08-19 09:19:47 -0500

    Added a col-row position to the history

--- src/components/game.component.js
+++ src/components/game.component.js
@@ -11,21 +11,42 @@ export default class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
+      position: [
+        {
+          col: null,
+          row: null
+        }
+      ],
       stepNumber: 0,
       isXNext: true
     };
+  }
+
+  getPosition(i) {
+    if (i >= 0 && i < 3) {
+      return { row: 1, col: i + 1 };
+    } else if (i >= 3 && i < 6) {
+      return { row: 2, col: i - 2 };
+    } else {
+      return { row: 3, col: i - 5 };
+    }
   }
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    const currentPosition = this.state.position.slice(0, this.state.stepNumber + 1);
+    const playedPosition = this.getPosition(i);
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
     squares[i] = this.state.isXNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{ squares }]),
+      position: currentPosition.concat(playedPosition),
       stepNumber: history.length,
       isXNext: !this.state.isXNext
     });
@@ -45,9 +66,16 @@ export default class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
+      const position = this.state.position[move];
+      const currentPosition = position.col
+        ? 'Col - ' + position.col + ', Row - ' + position.row
+        : '';
+
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}: {currentPosition}
+          </button>
         </li>
       );
     });
